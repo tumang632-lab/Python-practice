67444f87f3f6990d9fd9424833be3961ddd1a8b4
Umang Tiwari <tumang632@gmail.com>
2026-06-25 16:24:43 +0530

    Add files via upload

---
+++ calculator.py.py
@@ -0,0 +1,27 @@
+print("Simple Calculator")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter your choice (1-4): ")
+
+if choice in ['1', '2', '3', '4']:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(f"{num1} + {num2} = {num1 + num2}")
+        elif choice == '2':
+            print(f"{num1} - {num2} = {num1 - num2}")
+        elif choice == '3':
+            print(f"{num1} * {num2} = {num1 * num2}")
+        elif choice == '4':
+            if num2 != 0:
+                print(f"{num1} / {num2} = {num1 / num2}")
+            else:
+                print("Error: Division by zero!")
+        else:
+          print("Invalid choice!")
+
+
